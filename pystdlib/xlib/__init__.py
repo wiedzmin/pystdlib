cac555a66c5bf01e165ec19b45ad24bb9b86f341
Alex Ermolov <aaermolov@gmail.com>
2020-08-31 22:08:03 +0300

    xlib: added functions

--- pystdlib/xlib/__init__.py
+++ pystdlib/xlib/__init__.py
@@ -1,6 +1,13 @@
 import Xlib.protocol.event
 from Xlib import X, display, error, Xatom, Xutil
 
+from pystdlib import shell_cmd
+
+
+def prepare_desktops_map():
+    desktops = shell_cmd("wmctrl -d", split_output="\n")
+    return { desktop[-1]: desktop[0] for desktop in [desktop.split() for desktop in desktops]}
+
 
 def sendEvent(win, ctype, data, mask=None):
     data = (data+[0]*(5-len(data)))[:5]
@@ -16,3 +23,8 @@ def switch_desktop(index):
     sendEvent(display.screen().root, display.intern_atom("_NET_CURRENT_DESKTOP"),
               [index, X.CurrentTime])
     display.flush()
+
+
+def switch_named_desktop(name):
+    index = prepare_desktops_map()[name] # TODO: add more safety harness
+    switch_desktop(int(index))
