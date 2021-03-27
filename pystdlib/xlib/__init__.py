1de041e0c70d05560442fb78ca0b330fa97da322
Alex Ermolov <aaermolov@gmail.com>
2021-03-27 21:23:37 +0300

    xlib: added active window traits getter

--- pystdlib/xlib/__init__.py
+++ pystdlib/xlib/__init__.py
@@ -6,6 +6,12 @@ from Xlib import X, display, error, Xatom, Xutil
 from pystdlib import shell_cmd
 
 
+d = display.Display()
+root = d.screen().root
+_NET_ACTIVE_WINDOW = d.intern_atom("_NET_ACTIVE_WINDOW")
+_NET_WM_NAME = d.intern_atom("_NET_WM_NAME")
+
+
 def prepare_desktops_map():
     desktops = shell_cmd("wmctrl -d", split_output="\n")
     return { desktop[-1]: desktop[0] for desktop in [desktop.split() for desktop in desktops]}
@@ -36,3 +42,16 @@ def is_idle_enough(xprintidle_binary, idle_time_treshold=3600):
     idle_time = shell_cmd(xprintidle_binary, env={"DISPLAY": os.getenv("DISPLAY"),
                                                   "XAUTHORITY": os.getenv("XAUTHORITY")})
     return int(idle_time) >= idle_time_treshold * 1000
+
+
+def get_active_window_traits():
+    active_window = root.get_full_property(_NET_ACTIVE_WINDOW, X.AnyPropertyType).value.pop()
+    active_window_name = None
+    active_window_class = None
+    try:
+        active_window_obj = d.create_resource_object("window", active_window)
+        active_window_name = active_window_obj.get_full_property(_NET_WM_NAME, X.AnyPropertyType).value.decode("ascii")
+        active_window_class = active_window_obj.get_wm_class()[0] or ""
+    except XError:
+        pass
+    return active_window_name, active_window_class
