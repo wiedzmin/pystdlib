bb45de37440b0e9b36b99f6540008cc197bcd4ab
Alex Ermolov <aaermolov@gmail.com>
2021-04-11 23:50:30 +0300

    shell: allow to ignore some awaited return codes

--- pystdlib/__init__.py
+++ pystdlib/__init__.py
@@ -6,7 +6,7 @@ import sys
 def shell_cmd(cmd, oneshot=False, env=None, shell=True,
               split_output=None, universal_newlines=None, input=None,
               executable=None, stdin=None, stdout=subprocess.PIPE,
-              stderr=subprocess.PIPE, exit_error_codes=None):
+              stderr=subprocess.PIPE, exit_error_codes=None, ignore_error_codes=None):
     result = None
     if oneshot:
         os.system(cmd)
@@ -19,6 +19,8 @@ def shell_cmd(cmd, oneshot=False, env=None, shell=True,
     if completed.returncode != 0:
         if exit_error_codes and completed.returncode in exit_error_codes:
             sys.exit(1)
+        elif ignore_error_codes and completed.returncode in ignore_error_codes:
+            pass
         else:
             raise ValueError(f"'{cmd}' returned {completed.returncode}")
 
